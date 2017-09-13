98b5d76ab73fd67b0c8e69c0236b7072737ffa61
Herivelto Urbano <heriveltourbano@gmail.com>
2017-09-13 02:29:10 -0300

    Criado os fornecedores

--- src/Controller/C_Fornecedor.java
+++ src/Controller/C_Fornecedor.java
@@ -5,6 +5,7 @@
  */
 package Controller;
 
+import DAO.Fornecedor;
 import Util.JPAUtil;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -16,14 +17,75 @@ import javax.persistence.Query;
  */
 public class C_Fornecedor {
     
+    public void salvar(Fornecedor p){
+        
+        EntityManager em = new JPAUtil().getEntityManager();
+         try{
+            em.getTransaction().begin();
+            em.persist(p);
+            em.getTransaction().commit();
+         }catch(Exception ex){
+            em.getTransaction().rollback();
+         }finally{
+            em.close();
+         }    
+    }
     
+    public void atualizar(Fornecedor p){
+            EntityManager em = new JPAUtil().getEntityManager();
+            try{
+                em.getTransaction().begin();
+                em.merge(p);
+                em.getTransaction().commit();
+            }catch(Exception ex){
+                em.getTransaction().rollback();
+            }finally{
+                em.close();
+            }
+    }
     
-    
+     public boolean excluir(Integer id){
+            EntityManager em = new JPAUtil().getEntityManager();
+            try{
+                em.getTransaction().begin();
+                Fornecedor p = null;
+                p = em.find(Fornecedor.class, id);
+                if(p == null){
+                    return false;
+                }else{
+                    em.remove(p);
+                }
+                em.getTransaction().commit();
+            }catch(Exception ex){
+                em.getTransaction().rollback();
+            }finally{
+                em.close();
+            }
+            return true;
+    }
+   
     public List<Object[]> consulta(){
         EntityManager em = new JPAUtil().getEntityManager();
-        Query q = em.createNativeQuery("select * from produto");
+        Query q = em.createNativeQuery("select * from Fornecedor");
         List<Object[]> fornecedores = q.getResultList();
         return fornecedores;
+    }
+    public Fornecedor consulta(Integer id){
+        EntityManager em = new JPAUtil().getEntityManager();
+        Fornecedor p = null;
+            try{
+                em.getTransaction().begin(); 
+                p = em.find(Fornecedor.class, id);
+                if(p == null){
+                    return null;
+                }
+                em.getTransaction().commit();
+            }catch(Exception ex){
+                em.getTransaction().rollback();
+            }finally{
+                em.close();
+            }
+            return p;
     }
     
 }
